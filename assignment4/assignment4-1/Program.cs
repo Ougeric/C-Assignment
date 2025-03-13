c7c79901c6951e8bc66815ebb4a325a674a201f9
Yuheng Zhao <165012055+Ougeric@users.noreply.github.com>
2025-03-13 19:11:31 +0800

    Add files via upload

---
+++ assignment4/assignment4-1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class GenericLinkedList<T>
+{
+    private class Node
+    {
+        public T Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    private Node head;
+
+    public void Add(T data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        Node current = head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GenericLinkedList<int> list = new GenericLinkedList<int>();
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+        list.Add(5);
+        list.Add(15);
+
+        Console.WriteLine("链表元素：");
+        list.ForEach(x => Console.WriteLine(x));
+
+        int sum = 0;
+        list.ForEach(x => sum += x);
+        Console.WriteLine($"总和：{sum}");
+
+        int max = int.MinValue;
+        list.ForEach(x => { if (x > max) max = x; });
+        Console.WriteLine($"最大值：{max}");
+
+        int min = int.MaxValue;
+        list.ForEach(x => { if (x < min) min = x; });
+        Console.WriteLine($"最小值：{min}");
+    }
+}
---
+++ assignment4/assignment4-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Timers; 
+
+public class AlarmClock
+{
+    private readonly System.Timers.Timer _timer; 
+    public DateTime AlarmTime { get; set; }
+
+    public event EventHandler Tick;
+    public event EventHandler Alarm;
+
+    public AlarmClock(DateTime alarmTime)
+    {
+        AlarmTime = alarmTime;
+        _timer = new System.Timers.Timer(1000); 
+        _timer.Elapsed += TimerElapsed;
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
+    }
+
+    private void TimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        Tick?.Invoke(this, EventArgs.Empty);
+        if (DateTime.Now >= AlarmTime)
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+            _timer.Stop();
+        }
+    }
+
+    public void Start() => _timer.Start();
+}
+
+class Program
+{
+    static void Main()
+    {
+        var alarmTime = DateTime.Now.AddSeconds(10);
+        var clock = new AlarmClock(alarmTime);
+
+        clock.Tick += (sender, e) =>
+            Console.WriteLine($"[{DateTime.Now:T}] 滴答...");
+
+        clock.Alarm += (sender, e) =>
+            Console.WriteLine($"[{DateTime.Now:T}] 叮铃铃！闹钟时间到！");
+
+        Console.WriteLine($"闹钟已设置，将在 {alarmTime:T} 响铃");
+        clock.Start();
+        Console.ReadLine();
+    }
+}
